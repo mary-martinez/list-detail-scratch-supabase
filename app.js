13975da2ee306d7959651f34e25e91048a39e77a
Mary Martinez <mary.diana.martinez@gmail.com>
2022-02-10 15:40:45 -0800

    async fetchToys function works with renderToy to render the toys list to the home page

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-import { } from './fetch-utils.js';
+import { fetchToys } from './fetch-utils.js';
+import { renderToy } from './render.js';
+
+const shelves = document.querySelector('.shelves');
+
+window.addEventListener('load', async () => {
+    shelves.textContent = '';
+    const toys = await fetchToys();
+    // console.log(toys);
+    for (let toy of toys) {
+        const thisToy = renderToy(toy);
+        shelves.append(thisToy);
+    }
+});
--- fetch-utils.js
+++ fetch-utils.js
@@ -3,40 +3,11 @@ const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZ
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export async function getUser() {
-    return client.auth.session() && client.auth.session().user;
+export async function fetchToys() {
+    const resp = await client.from('toys').select('*');
+    return checkError(resp);
 }
-
-
-export async function checkAuth() {
-    const user = await getUser();
-
-    if (!user) location.replace('../'); 
-}
-
-export async function redirectIfLoggedIn() {
-    if (await getUser()) {
-        location.replace('./other-page');
-    }
-}
-
-export async function signupUser(email, password){
-    const response = await client.auth.signUp({ email, password });
-    
-    return response.user;
-}
-
-export async function signInUser(email, password){
-    const response = await client.auth.signIn({ email, password });
-
-    return response.user;
-}
-
-export async function logout() {
-    await client.auth.signOut();
-
-    return window.location.href = '../';
-}
+// console.log(await fetchToys());
 
 function checkError({ data, error }) {
     return error ? console.error(error) : data;
